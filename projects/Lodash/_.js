123038a85b5ed870ffd85e36331386ede6ef8da7
Cody Brobston <cbrobston22@gmail.com>
2020-07-13 13:23:33 -0700

    First commit.

---
+++ projects/Lodash/_.js
@@ -0,0 +1,60 @@
+const _ = {
+    clamp(number, lowerBound, upperBound) {
+        if (number <= lowerBound) {
+            return lowerBound;
+        } else if (number >= upperBound) {
+            return upperBound;
+        } else {
+            return number;
+        }
+    },
+    inRange(number, start, end) {
+        if (end === undefined) {
+            end = start;
+            start = 0;
+        }
+        if (start > end) {
+            let temp = start;
+            start = end;
+            end = temp;
+        }
+        if (number < start) {
+            return false;
+        } else if (number >= end) {
+            return false;
+        } else {
+            return true;
+        }
+    },
+    words(stringOfWords) {
+        const arrOfWords = stringOfWords.split(' ');
+        return arrOfWords;
+    },
+    pad(string, length) {
+        let lengthDiff = length - string.length;
+        if (lengthDiff > 0) {
+            let startPadding = Math.floor(lengthDiff/2);
+            let endPadding = lengthDiff - startPadding;
+            let padding = ' ';
+            string = padding.repeat(startPadding) + string + padding.repeat(endPadding);
+        }
+        return string;
+    },
+    has(object, key) {
+        let hasKey = object[key] !== undefined;
+        return hasKey;
+    },
+    invert(object) {
+        let keys = Object.keys(object).reverse();
+        let values = Object.values(object).reverse();
+        let newObject = {};
+        for (let i=0; i<keys.length; i++) {
+            newObject[values[i]] = keys[i];
+        }
+        return newObject;
+    }
+};
+
+
+// Do not write or modify code below this line.
+module.exports = _;
