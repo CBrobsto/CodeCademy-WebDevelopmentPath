03132219593fa6c3000f039e6375b65cf30b6f01
Cody Brobston <cbrobston22@gmail.com>
2020-07-27 09:05:32 -0700

    Refactored the files to use both default and named import/exports. The program still errors locally but functions in CodeCademy's browser IDE.

--- projects/WorkAround/employee.js
+++ projects/WorkAround/employee.js
@@ -8,7 +8,7 @@ let payGrades = {
   seniorLevel: { taxMultiplier: .2, benefits: ['health', 'housing', 'wellness', 'gym'], minSalary: 100000, maxSalary: 200000 }
 };
 
-Employee.getCadre = function() {
+export function getCadre() {
   if (Employee.salary >= payGrades.entryLevel.minSalary && Employee.salary <= payGrades.entryLevel.maxSalary) {
     return 'entryLevel';
   } else if (Employee.salary >= payGrades.midLevel.minSalary && Employee.salary <= payGrades.midLevel.maxSalary) {
@@ -16,22 +16,22 @@ Employee.getCadre = function() {
   } else return 'seniorLevel';
 }
 
-Employee.calculateTax = function() {
-  return payGrades[Employee.getCadre()].taxMultiplier * Employee.salary;
+export function calculateTax() {
+  return payGrades[getCadre()].taxMultiplier * Employee.salary;
 }
 
-Employee.getBenefits = function() {
-  return payGrades[Employee.getCadre()].benefits.join(', ');
+export function getBenefits() {
+  return payGrades[getCadre()].benefits.join(', ');
 }
 
-Employee.calculateBonus = function() {
+export function calculateBonus() {
   return .02 * Employee.salary;
 }
 
-Employee.reimbursementEligibility = function() {
+export function reimbursementEligibility() {
   let reimbursementCosts = { health: 5000, housing: 8000, wellness: 6000, gym: 12000 };
   let totalBenefitsValue = 0; 
-  let employeeBenefits = payGrades[Employee.getCadre()].benefits;
+  let employeeBenefits = payGrades[getCadre()].benefits;
   for (let i = 0; i < employeeBenefits.length; i++) {
     totalBenefitsValue += reimbursementCosts[employeeBenefits[i]];
   }
